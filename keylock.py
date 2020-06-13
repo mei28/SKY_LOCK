6c16602b3a4d805c636947616c71fb806957f648
meitetsu <mei28aquarius@gmail.com>
2020-06-13 08:21:26 +0100

    fix type

---
+++ keylock.py
@@ -0,0 +1,20 @@
+import time 
+import sys
+import sg5010
+
+s = sg5010.Sg5010(4,0)
+
+opened = sys.argv[1]
+
+print(opened)
+if(opened==1 or opened=='1'):
+    print("Opening ...")
+    s.setdirection(100,10)
+elif(opened==0 or opened=='0'):
+    print("Closing ...")
+    s.setdirection(-100,-10)
+else:
+    print("Error")
+
+time.sleep(0.5)
+print("Good Bye")
