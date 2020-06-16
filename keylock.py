364f21ec977d69cffecbec0659a75866b2f34dfa
meitetsu <mei28aquarius@gmail.com>
2020-06-16 07:19:43 +0100

    fix parameter

--- keylock.py
+++ keylock.py
@@ -2,17 +2,17 @@ import time
 import sys
 import sg5010
 
-s = sg5010.Sg5010(4,0)
-
+s = sg5010.Sg5010(4,100)
+time.sleep(0.5)
 opened = sys.argv[1]
 
 print(opened)
 if(opened==1 or opened=='1'):
     print("Opening ...")
-    s.setdirection(100,10)
+    s.setdirection(-10,-10)
 elif(opened==0 or opened=='0'):
     print("Closing ...")
-    s.setdirection(-100,-10)
+    s.setdirection(100,10)
 else:
     print("Error")
 
